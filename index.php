90c4c66acd3eeaee178d3f0d69e57798e014e4e7
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-03 16:45:31 +0200

    feat: create variables and construct

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
 
+// Creazione classe Movie
+
+class Movie{
+
+    // Variabili d'istanza
+
+    public $title;
+    public $director;
+    public $genre;
+    public $year;
+    public $leading_actors;
+    public $duration;
+    public $original_language;
+
+    // Costruttore
+
+    function _construct($_title, $_director, $_genre, $_year, $_leading_actors, $_duration, $_original_language){
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->genre = $_genre;
+        $this->year = $_year;
+        $this->leading_actors = $_leading_actors;
+        $this->duration = $_duration;
+        $this->original_language = $_original_language;
+    }
+
+    
+}
+
+
 ?>
 
 
