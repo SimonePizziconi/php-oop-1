97bd41750f9e9292f339ce1c5418e5b086742b16
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-03 16:58:49 +0200

    feat: create method and print in page two films

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ class Movie{
 
     // Costruttore
 
-    function _construct($_title, $_director, $_genre, $_year, $_leading_actors, $_duration, $_original_language){
+    function __construct($_title, $_director, $_genre, $_year, $_leading_actors, $_duration, $_original_language){
         $this->title = $_title;
         $this->director = $_director;
         $this->genre = $_genre;
@@ -26,9 +26,24 @@ class Movie{
         $this->original_language = $_original_language;
     }
 
-    
+    public function getMovieDetails() {
+        return "<ul>" .
+               "<li><strong>Titolo:</strong> " . $this->title . "</li>" .
+               "<li><strong>Regista:</strong> " . $this->director . "</li>" .
+               "<li><strong>Genere:</strong> " . $this->genre . "</li>" .
+               "<li><strong>Anno:</strong> " . $this->year . "</li>" .
+               "<li><strong>Attori protagonisti:</strong> " . $this->leading_actors . "</li>" .
+               "<li><strong>Durata:</strong> " . $this->duration . " minutes</li>" .
+               "<li><strong>Lingua Originale:</strong> " . $this->original_language . "</li>" .
+               "</ul>";
+    }
 }
 
+$movie1 = new Movie("Happy Gilmore", "Dennis Dugan", "Comedy", 1996, "Adam Sandler, Christopher McDonald", 92, "English");
+$movie2 = new Movie("Uncut Gems", "Josh Safdie, Benny Safdie", "Thriller", 2019, "Adam Sandler, Julia Fox", 135, "English");
+
+echo $movie1->getMovieDetails();
+echo $movie2->getMovieDetails();
 
 ?>
 
